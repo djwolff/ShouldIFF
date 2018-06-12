51e6d9c6aafa4871aa981d87a24fad756b952a8b
David Wolff <davidwolff2020@u.northwestern.edu>
2018-06-12 02:32:20 -0500

    Make a predictor depending on the game.

--- learner.py
+++ learner.py
@@ -23,16 +23,15 @@ from sklearn.svm import SVC
 from plotting import plot_learning_curve
 
 ## Get data
-dataset = pandas.read_csv('Data/stats2.csv')
-
+dataset = pandas.read_csv('Data/seasons/season8.csv')
 
 # To determine how many samples we want.
-n = 3000
+n = 100
 
 # X = data and y = target
 array = dataset.values
-X = array[:n,1:]
-y = array[:n,0]
+X = array[:n,1:]  	# The data and attributes
+y = array[:n,0]		# The targets
 
 ## Initialization of our data
 validation_size = 0.2
@@ -42,18 +41,18 @@ X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(
 
 
 ## Debugging issues
-print("Classifier: %s" % dataset.columns.values[0])
-print("Attributes: %s" % dataset.columns.values[1:])
+# print("Classifier: %s" % dataset.columns.values[0])
+# print("Attributes: %s" % dataset.columns.values[1:])
 
 
 ## Create accuracies for the models.
 models = []
-models.append(('LR', LogisticRegression()))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
+# models.append(('LR', LogisticRegression()))
+# models.append(('LDA', LinearDiscriminantAnalysis()))
+# models.append(('KNN', KNeighborsClassifier()))
 models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC()))
+# models.append(('NB', GaussianNB()))
+# models.append(('SVM', SVC()))
 
 # evaluate each model in turn
 results = []
@@ -82,3 +81,124 @@ for name, model in models:
 	plot_learning_curve(model, title, X, y, ylim=(0.3, 1.01), cv=cv, n_jobs=1)
 
 	plt.show()
+
+
+
+## From the models above, we choose DecisionTreeClassifierself.
+estimator = DecisionTreeClassifier(random_state=0)
+estimator.fit(X_train, Y_train)
+
+# The decision estimator has an attribute called tree_  which stores the entire
+# tree structure and allows access to low level attributes. The binary tree
+# tree_ is represented as a number of parallel arrays. The i-th element of each
+# array holds information about the node `i`. Node 0 is the tree's root. NOTE:
+# Some of the arrays only apply to either leaves or split nodes, resp. In this
+# case the values of nodes of the other type are arbitrary!
+#
+# Among those arrays, we have:
+#   - left_child, id of the left child of the node
+#   - right_child, id of the right child of the node
+#   - feature, feature used for splitting the node
+#   - threshold, threshold value at the node
+#
+
+# Using those arrays, we can parse the tree structure:
+
+n_nodes = estimator.tree_.node_count
+children_left = estimator.tree_.children_left
+children_right = estimator.tree_.children_right
+feature = estimator.tree_.feature
+threshold = estimator.tree_.threshold
+
+
+# The tree structure can be traversed to compute various properties such
+# as the depth of each node and whether or not it is a leaf.
+node_depth = numpy.zeros(shape=n_nodes, dtype=numpy.int64)
+is_leaves = numpy.zeros(shape=n_nodes, dtype=bool)
+stack = [(0, -1)]  # seed is the root node id and its parent depth
+while len(stack) > 0:
+    node_id, parent_depth = stack.pop()
+    node_depth[node_id] = parent_depth + 1
+
+    # If we have a test node
+    if (children_left[node_id] != children_right[node_id]):
+        stack.append((children_left[node_id], parent_depth + 1))
+        stack.append((children_right[node_id], parent_depth + 1))
+    else:
+        is_leaves[node_id] = True
+
+print("The binary tree structure has %s nodes and has "
+      "the following tree structure:"
+      % n_nodes)
+for i in range(n_nodes):
+    if is_leaves[i]:
+        print("%snode=%s leaf node." % (node_depth[i] * "\t", i))
+    else:
+        print("%snode=%s test node: go to node %s if X[:, %s] <= %s else to "
+              "node %s."
+              % (node_depth[i] * "\t",
+                 i,
+                 children_left[i],
+                 feature[i],
+                 threshold[i],
+                 children_right[i],
+                 ))
+print()
+
+# First let's retrieve the decision path of each sample. The decision_path
+# method allows to retrieve the node indicator functions. A non zero element of
+# indicator matrix at the position (i, j) indicates that the sample i goes
+# through the node j.
+
+node_indicator = estimator.decision_path(X_validation)
+
+# Similarly, we can also have the leaves ids reached by each sample.
+
+leave_id = estimator.apply(X_validation)
+
+# Now, it's possible to get the tests that were used to predict a sample or
+# a group of samples. First, let's make it for the sample.
+
+sample_id = 0
+node_index = node_indicator.indices[node_indicator.indptr[sample_id]:
+                                    node_indicator.indptr[sample_id + 1]]
+
+print('Rules used to predict sample %s: ' % sample_id)
+for node_id in node_index:
+    if leave_id[sample_id] != node_id:
+        continue
+
+    if (X_validation[sample_id, feature[node_id]] <= threshold[node_id]):
+        threshold_sign = "<="
+    else:
+        threshold_sign = ">"
+
+    print("decision id node %s : (X_validation[%s, %s] (= %s) %s %s)"
+          % (node_id,
+             sample_id,
+             feature[node_id],
+             X_validation[sample_id, feature[node_id]],
+             threshold_sign,
+             threshold[node_id]))
+
+# For a group of samples, we have the following common node.
+sample_ids = [0, 1]
+common_nodes = (node_indicator.toarray()[sample_ids].sum(axis=0) ==
+                len(sample_ids))
+
+common_node_id = numpy.arange(n_nodes)[common_nodes]
+
+print("\nThe following samples %s share the node %s in the tree"
+      % (sample_ids, common_node_id))
+print("It is %s %% of all nodes." % (100 * len(common_node_id) / n_nodes,))
+
+
+######## Now to predict!!!
+
+ourGame = numpy.array([dataset.values[len(dataset.values)-2,1:]]) # Choose a datavalue that we haven't seen.
+result = estimator.predict(ourGame)
+
+if result[0] = 0:
+	print("If the game is prolonged, your team will lose. It is wise to surrender now.")
+if result[0] = 1:
+	print("Don't give up! Your team is likely to win this!")
